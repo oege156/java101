460a27ba50d430b86def5d211fc912d8b31681e4
oege156 <oege156@gmail.com>
2022-07-23 14:59:09 +0300

    daireAlanCevre

--- daireAlanCevre/src/daireAlanCevre.java
+++ daireAlanCevre/src/daireAlanCevre.java
@@ -3,19 +3,27 @@ public class daireAlanCevre {
     public static void main(String[] args) {
 
 
-        double r , pi = 3.14 , alan , cevre ;
+        double a , r , pi = 3.14 , alan , cevre , dilimAlan ;
 
         Scanner inp = new Scanner(System.in);
 
         System.out.println("Dairenin yaricapini giriniz :");
         r = inp.nextDouble();
 
+        System.out.println("Merkez acisinin olcusunu giriniz :");
+        a = inp.nextDouble();
+
+
         alan = pi * r * r;
 
         cevre = 2 * pi * r;
 
+        dilimAlan = (pi * (r*r) * a) / 360;
+
         System.out.println("Dairenin alani : "+alan);
         System.out.println("Dairenin cevresi : "+cevre);
+        System.out.println("Daire diliminin alani : "+dilimAlan);
+
 
     }
 }
