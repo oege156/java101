2e39a6f8532f6073441c00a92bb51ef801d46b08
oege156 <oege156@gmail.com>
2022-07-27 14:56:47 +0300

    harmonikSeriBulma düzenlendi

--- harmonikSeriBulma/src/harmonikSeriBulma.java
+++ harmonikSeriBulma/src/harmonikSeriBulma.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class harmonikSeriBulma {
+import java.util.Scanner;
+public class harmonikSeriBulma {
+    public static void main(String[] args) {
+        double result = 0,i;
+        int n;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("n sayisini giriniz :");
+        n = inp.nextInt();
+
+        for (i =1;i<=n;i++) {
+            result += (1/i);
+        }
+        System.out.println(result);
+    }
 }
