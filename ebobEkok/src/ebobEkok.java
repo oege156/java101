d3ef933f07954f653c04070f3f1b3f95b25c9702
oege156 <oege156@gmail.com>
2022-07-30 12:28:26 +0300

    ebobEkok eklendi

---
+++ ebobEkok/src/ebobEkok.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ebobEkok {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("n1 sayisini giriniz");
+        int n1 = inp.nextInt();
+        System.out.println("n2 sayisini giriniz");
+        int n2 = inp.nextInt();
+        int ebob=1;
+        int i = 1;
+        while (i <= n1) {
+            if (n1 % i ==0 && n2 % i ==0) {
+                ebob =i;
+
+            }
+            i++;
+        }
+        System.out.println("ebob :"+ebob);
+        System.out.println("ekok :"+(n1*n2)/ebob);
+
+
+    }
+}
