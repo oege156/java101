2f6e9beca7a2b5a3be15b20eb43cb028f126b1ae
oege156 <oege156@gmail.com>
2022-08-01 12:36:21 +0300

    recursiveFibonacci added

--- recursiveFibonacci/src/recursiveFibonacci.java
+++ recursiveFibonacci/src/recursiveFibonacci.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 public class recursiveFibonacci {
+    static int fib(int n) {
+        if (n==1 || n==2){
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
     public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("What digit of the Fibonacci series do you want to find?");
+        int digits = inp.nextInt();
+        System.out.println(fib(digits));
 
     }
 }
